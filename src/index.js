8f8d545f540360086baccc3604a9a9a6ea3c4297
Zhiyuan He <zh2312@columbia.edu>
2019-08-21 17:41:48 -0400

    change square to function component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+// square become a function component so no 'render' 
+function Square(props) {
 
-  // render in each component
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-          {this.props.value}
-      </button>
-    );
-  }
+  // instead of 'render', it 'returns' object as functions do
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -28,8 +24,10 @@ class Board extends React.Component {
   // functions used in render()
   // called when clicked
   handleClick(i) {
+    // use slice to create a copy of squares array
     const squares = this.state.squares.slice();
     squares[i] = 'X';
+    // set 'squares' of 'Board' to be the new array
     this.setState({squares: squares});
   }
 
