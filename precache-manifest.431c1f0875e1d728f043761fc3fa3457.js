83de3edae6fcb34ef66ca9d7e6bb0db97edef2a9
Zhiyuan He <zh2312@columbia.edu>
2019-08-22 12:41:30 -0400

    Updates

---
+++ precache-manifest.431c1f0875e1d728f043761fc3fa3457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef855fa24982de9069c60ccdb04b04d",
+    "url": "/react-tryout/index.html"
+  },
+  {
+    "revision": "a4e6c9383dc0672257dd",
+    "url": "/react-tryout/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "7955b232cd5dd2518f98",
+    "url": "/react-tryout/static/js/2.6255ca29.chunk.js"
+  },
+  {
+    "revision": "a4e6c9383dc0672257dd",
+    "url": "/react-tryout/static/js/main.8b9cb342.chunk.js"
+  },
+  {
+    "revision": "632953886dad6ff0011f",
+    "url": "/react-tryout/static/js/runtime~main.41ba5450.js"
+  }
+]);
